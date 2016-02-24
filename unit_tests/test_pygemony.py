9d5a354a99fb99a58f62cf9c82022a2d71e7544a
GrappigPanda <Ian@ianleeclark.com>
2016-02-23 19:06:46 -0600

    Issue #139: Removing codesmells indicated by landscape.io

--- unit_tests/test_pygemony.py
+++ unit_tests/test_pygemony.py
@@ -7,5 +7,5 @@ from pyg.Pygemony import Pygemony
 class PygemonyTestCase(unittest.TestCase):
 
     def test_hashing_pass(self):
-        md5 = Pygemony.hash_todo("# TODO(ian): Testing 123", 5, 'test.py')
+        md5 = Pygemony.hash_todo("# TODO(ian): Testing 123", 'test.py')
         self.assertEqual(md5, "8f83bdfe5ce85ac91d3e84e879fce24e")
