ca17df30a6e3786c58ee161294512fd0fe7e4199
ian Clark <ian.clark@virtucrypt.com>
2016-02-26 14:45:21 -0600

    Issue #147: Added git branch detection and a new wrapper which verifies we're in a valid git repo

--- pyg/utils.py
+++ pyg/utils.py
@@ -1,4 +1,22 @@
+from __future__ import print_function
 from mimetypes import guess_type
+from functools import wraps
+
+import os
+
+
+class InvalidGitRepo(Exception):
+    def __init__(*args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+def in_git_repo(function):
+    @wraps(function)
+    def _wrapper(*args, **kwargs):
+        if os.path.isdir("./.git"):
+            return function(*args, **kwargs)
+        raise InvalidGitRepo("Current directory is not a valid git repo!")
+    return _wrapper
+
 
 def get_git_info():
     repo = ''
@@ -13,3 +31,15 @@ def get_git_info():
 
 def detect_mimetype(file_):
     return guess_type(file_)
+
+
+@in_git_repo
+def detect_curr_branch():
+    with open('.git/HEAD', 'r') as f:
+        try:
+            return f.readline().split("heads/")[-1].strip()
+        except IOError:
+            raise InvalidGitRepo("Cannot locate .git/HEAD")
+        except IndexError:
+            raise InvalidGitRepo("Invalid HEAD file, no branch found")
+
